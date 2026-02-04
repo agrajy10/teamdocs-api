68e350bba5643d3618a96626cb31d5c39f5e686a
Agraj Yadav <agrajy10@gmail.com>
2026-02-04 11:45:08 +0530

    test: add team association to logout test setup

--- tests/integration/logout.test.js
+++ tests/integration/logout.test.js
@@ -2,15 +2,18 @@ import request from "supertest";
 import app from "../helpers/testApp";
 import { begin, rollback } from "../helpers/db";
 import seedUser from "../helpers/seedUser";
+import seedTeam from "../helpers/seedTeam";
 
+let teamId;
 beforeEach(async () => {
   await begin();
+  teamId = await seedTeam();
 });
 
 const endpoint = "/auth/logout";
 describe(`POST ${endpoint}`, () => {
   it("deletes an existing session", async () => {
-    await seedUser();
+    await seedUser({ teamId });
     const loginResponse = await request(app).post("/auth/login").send({
       email: "test@example.com",
       password: "StrongPass1!",
