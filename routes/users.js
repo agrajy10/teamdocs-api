234f6cfdf70acd5229f28f2fe88126dcd120c2c5
Agraj Yadav <agrajy10@gmail.com>
2026-02-07 11:11:45 +0530

    refactor(routes): remove unused middleware imports from users.js

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,7 @@ import authorize from "../middleware/authorize.js";
 import { deleteUser, getAllUsers } from "../services/users.js";
 import authenticateSession from "../middleware/authenticateSession.js";
 import checkUserInTeam from "../middleware/checkUserInTeam.js";
-import { query } from "express-validator";
 import handleValidationError from "../middleware/validation.middleware.js";
-import isTeamAdmin from "../middleware/isTeamAdmin.js";
 
 const router = Router();
 
